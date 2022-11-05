9f9ab0aa7af1521fe4cf29f6f0fb0ae4929427e3
xiaomingk <47573182+xiaomingk@users.noreply.github.com>
2022-11-05 10:09:46 +0100

    Update inputdata.jl

--- src/inputdata.jl
+++ src/inputdata.jl
@@ -202,7 +202,7 @@ function makeparameters(sets, options, hourinfo)
     end
 
     # from Bogdanov & Breyer (2016) "North-East Asian Super Grid..."
-    transmissioncostdata = connected .* (150 .+ 0.4*distances) .+ connectedoffshore .* (150 .+ 0.47*distances)
+    transmissioncostdata = connected .* (75 .+ 0.2*distances) .+ connectedoffshore .* (75 .+ 0.2355*distances)
     transmissionfixedcostdata = connected .* (3 .+ 0.008*distances) .+ connectedoffshore .* (3 .+ 0.001645*distances)
     transmissioninvestcost = AxisArray(transmissioncostdata, REGION, REGION)        # €/kW
     transmissionfixedcost = AxisArray(transmissionfixedcostdata, REGION, REGION)        # €/kW
