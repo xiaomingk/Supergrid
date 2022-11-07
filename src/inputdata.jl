b7b7027650de06aa950307d58a56bcbb7e8ca301
xiaomingk <47573182+xiaomingk@users.noreply.github.com>
2022-11-07 20:51:19 +0100

    Update inputdata.jl

--- src/inputdata.jl
+++ src/inputdata.jl
@@ -230,7 +230,7 @@ function makeparameters(sets, options, hourinfo)
         :wind           825         0               33          25          1           1
         :offwind        1700        0               55          25          1           1
         :transmission   NaN         0               NaN         40          NaN         1
-        :battery        116         0.1             1.5         10          0.9         1   # 1h discharge time, 150 €/kW = 150 €/kWh
+        :battery        76         0.1             1.5         10          0.9         1   # 1h discharge time, 150 €/kW = 150 €/kWh
         :pv             481         0               8           25          1           1
         :pvroof         581         0               6           25          1           1
         :csp            3746        2.9             56          30          1           1   # for solar multiple=3, storage=12 hours
