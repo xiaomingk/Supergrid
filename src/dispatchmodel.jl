a927a2f7255f5bbce290f19f0774ab6218ad7991
xiaomingk <47573182+xiaomingk@users.noreply.github.com>
2023-03-25 16:46:33 +0100

    h2

--- src/dispatchmodel.jl
+++ src/dispatchmodel.jl
@@ -101,7 +101,7 @@ function makedispatchconstraints(capacity, transmissioncapacity, m, sets, params
 
         HydroDemand[r in REGION],
            #sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 0.5 * sum(demand[r,h] for h in HOUR) * hoursperperiod
-            sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 10000
+            sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 5000
 
         HydrogenIN[r in REGION, h in HOUR],
             Charging[r,:hydrogen,h] <= Electricity[r,:electrolyzer,:_,h]
