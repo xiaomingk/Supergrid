2c5df43e8bf3efa852224c0b7300678a91287b6a
xiaomingk <47573182+xiaomingk@users.noreply.github.com>
2023-04-16 20:20:48 +0200

    h2

--- src/jumpmodel.jl
+++ src/jumpmodel.jl
@@ -163,7 +163,7 @@ function makeconstraints(m, sets, params, vars, hourinfo, options)
 
         HydroDemand[r in REGION],
             #sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 0.5 * sum(demand[r,h] for h in HOUR) * hoursperperiod
-            sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 16000000
+            sum(AnnualGeneration[r,k] for k in [:electrolyzer]) - sum(Charging[r,:hydrogen,h] for h in HOUR) == 14000000
         HydrogenIN[r in REGION, h in HOUR],
             Charging[r,:hydrogen,h] <= Electricity[r,:electrolyzer,:_,h]
 
